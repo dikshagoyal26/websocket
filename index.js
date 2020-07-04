08e50260a5db84e4bf262e3c4eabc72de36d8e5c
Diksha <diksha.goyal@intelliticks.com>
2020-07-04 16:07:27 +0530

    basic setup at backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const socket = require('socket.io')
+const app = express() //app setup
+const port = process.env.port || 8000
+
+app.use(express.static('public'));
+
+const io = socket(server); //io setup
+
+// Listening for event 'connection' with browser
+// Every connection will have a separate socket
+io.on('connection', function (socket) {
+    console.log("socket connction made");
+    socket.on('chat', function (data) {
+        //io.sockets is all sockets/connections with server
+        io.sockets.emit('chat', data);
+    })
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    })
+})
+
+const server = app.listen(port, () => {
+    console.log(`listening to ${port}`);
+})
